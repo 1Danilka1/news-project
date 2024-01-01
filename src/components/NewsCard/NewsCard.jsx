a8f2a9d0660b99f2818f17e68677a6c610208668
Danyl Brahan <danylbragan@gmail.com>
2024-01-01 01:00:02 +0100

    dfsdfsd

--- src/components/NewsCard/NewsCard.jsx
+++ src/components/NewsCard/NewsCard.jsx
@@ -1,28 +1,32 @@
-import PropTypes from 'prop-types';
-import css from './NewsCard.module.css'
+import PropTypes from "prop-types";
+import css from "./NewsCard.module.css";
 
 export default function NewsCard({ title, author, about, urlTo, image }) {
   return (
     <div className={css.container}>
-        <div>
-            <img className={css.img} src={image} alt="image-of-news" />
-        </div>
-        <div>
-            <h1 className={css.title}>{title}</h1>
-            <p className={css.text}>{about}</p>
-            <p className={css.text}>Author: {author}</p>
-        </div>
-        <div>
-            <a href={urlTo} target='blank'>Read more</a>
-        </div>
+      <div>
+        <img className={css.img} src={image} alt="image-of-news" />
+      </div>
+      <div>
+        <h1 className={css.title}>{title}</h1>
+        <p className={css.text}>{about}</p>
+        <p className={css.text}>Author: {author}</p>
+      </div>
+      <div>
+        <button className={css.btn}>
+          <a href={urlTo} target="blank" className={css.link_to_info}>
+            Read more
+          </a>
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
 NewsCard.propTypes = {
-    title: PropTypes.string,
-    author: PropTypes.string,
-    about: PropTypes.string,
-    urlTo: PropTypes.string,
-    image: PropTypes.string,
-}
+  title: PropTypes.string,
+  author: PropTypes.string,
+  about: PropTypes.string,
+  urlTo: PropTypes.string,
+  image: PropTypes.string,
+};
