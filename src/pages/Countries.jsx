00d633f5daa7bafe8519c19f545a25e6bedd0298
Danyl Brahan <danylbragan@gmail.com>
2024-01-15 15:13:57 +0100

    dsfsdfs

--- src/pages/Countries.jsx
+++ src/pages/Countries.jsx
@@ -7,46 +7,78 @@ export default function Countries() {
   const [selectedCountry, setSelectedCountry] = useState("");
   const [newsFromCountries, setNewsFromCountries] = useState([]);
 
+  const options = [
+    {
+      label: "--Select the country to read news--",
+      value: "nothing",
+    },
+    {
+      label: "USA",
+      value: "us",
+    },
+    {
+      label: "Germany",
+      value: "de",
+    },
+    {
+      label: "Ukraine",
+      value: "ua",
+    },
+    {
+      label: "France",
+      value: "fr",
+    },
+    {
+      label: "Czech Republic",
+      value: "cz",
+    },
+    {
+      label: "Canada",
+      value: "ca",
+    },
+    {
+      label: "Japan",
+      value: "jp",
+    },
+  ];
+
+  // ================================== fetch ==========================================================
   const fetchNewsFromCountries = () => {
-    fetch(
-      `https://newsapi.org/v2/top-headlines?country=us&apiKey=d526f51e3d9a4f0dba138594e06c3b1f`
-    )
-    
-      .then((response) => response.json())
-      .then((data) => {
-        const articles = data.articles || [];
-        setNewsFromCountries(articles);
-      })
-      .catch((error) => {
-        console.log(error);
-      })
+    if (selectedCountry) {
+      fetch(
+        `https://newsapi.org/v2/top-headlines?country=${selectedCountry}&apiKey=d526f51e3d9a4f0dba138594e06c3b1f`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          const articles = data.articles || [];
+          setNewsFromCountries(articles);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
   };
 
   useEffect(() => {
     fetchNewsFromCountries();
-  }, [])
+  }, [selectedCountry]);
+  // ================================================================================================
 
-  const hadleChange = (e) => {
+  function handleSubmit(e) {
     setSelectedCountry(e.target.value);
-  };
-
-  console.log(selectedCountry);
+  }
 
   return (
     <div className={css.container}>
       <main className={css.main}>
         Countries
         <div>
-          <select
-            name="selectCountry"
-            value={selectedCountry}
-            onChange={hadleChange}
-          >
-            <option value="USA">USA</option>
-            <option value="France">France</option>
-            <option value="Spain">Spain</option>
-            <option value="Japan">Japan</option>
-            <option value="Ukraine">Ukraine</option>
+          <select onChange={handleSubmit}>
+            {options.map((option) => (
+              <option key={option.value} value={option.value}>
+                {option.label}
+              </option>
+            ))}
           </select>
         </div>
         <div>
@@ -57,10 +89,7 @@ export default function Countries() {
                   to={`/${article.source.id}`}
                   state={{ from: location, article: article }}
                 >
-                  <NewsCard
-                      title={article.title}
-                      image={article.urlToImage}
-                    />
+                  <NewsCard title={article.title} image={article.urlToImage} />
                 </Link>
               </li>
             ))}
