dcbd4e86674ee8d09684f1cefc7b39b650965d23
Danyl Brahan <danylbragan@gmail.com>
2025-12-15 21:11:51 +0100

    dfndfd

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,30 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/news-project/',
+//   build: {
+//     outDir: 'dist',  // по умолчанию
+//     assetsDir: 'assets', // по умолчанию
+//     sourcemap: false,
+//     rollupOptions: {}
+//   }
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/news-project/',
+  base: '/news-project/', // имя твоего репозитория на GitHub
   build: {
-    outDir: 'dist',  // по умолчанию
-    assetsDir: 'assets', // по умолчанию
-    sourcemap: false,
-    rollupOptions: {}
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash][extname]'
+      }
+    }
   }
 })
