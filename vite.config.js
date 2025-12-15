4b49375af5b7005f6d405737e940715b12423999
Danyl Brahan <danylbragan@gmail.com>
2025-12-15 21:07:57 +0100

    dfndfd

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/news-project/'
+  base: '/news-project/',
+  build: {
+    outDir: 'dist',  // по умолчанию
+    assetsDir: 'assets', // по умолчанию
+    sourcemap: false,
+    rollupOptions: {}
+  }
 })
